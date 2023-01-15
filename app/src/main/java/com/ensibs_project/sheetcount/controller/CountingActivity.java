4ed8408c0f22e109179303ac069450a7fcd34aa9
Axel Tremaudant <axel.tremaudant@gmail.com>
2023-01-15 17:26:24 +0100

    Add the choice of the picture since the gallery

--- app/src/main/java/com/ensibs_project/sheetcount/controller/CountingActivity.java
+++ app/src/main/java/com/ensibs_project/sheetcount/controller/CountingActivity.java
@@ -9,6 +9,7 @@ package com.ensibs_project.sheetcount.controller;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.icu.text.SimpleDateFormat;
@@ -37,7 +38,9 @@ public class CountingActivity extends AppCompatActivity {
     private ImageView imageViewer;
     private Button pictureButton;
     private String photoPath;
+    private Button galleryButton;
     private static final int BACK_TAKE_PICTURE = 1;
+    private static final int BACK_CHOOSE_PICTURE = 2;
 
     /**
      * Set the items
@@ -50,10 +53,30 @@ public class CountingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_counting);
 
         pictureButton = findViewById(R.id.button);
-        pictureButton.setEnabled(true);
+        galleryButton = findViewById(R.id.galleryBtn);
+
         imageViewer = findViewById(R.id.imageView);
 
         pictureButton.setOnClickListener(view -> takePicture());
+        galleryButton.setOnClickListener(view -> choosePicture());
+    }
+
+    /**
+     * Function called to choose a picture in the gallery.
+     * NOTA: SheetCount must be allowed in 'Files and media permission' in 'Settings' of the phone.
+     */
+    private void choosePicture(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+
+        // Define thee type as image/*.
+        // This guarantee than just image type components are selected
+        intent.setType("image/*");
+        // We give an array with MIME types accepted
+        // This guarantee than just MIME type components are targeted
+        String[] mimeTypes = {"image/jpeg", "image/png","image/jpg"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+
+        startActivityForResult(intent, BACK_CHOOSE_PICTURE);
     }
 
     /**
@@ -68,6 +91,7 @@ public class CountingActivity extends AppCompatActivity {
         @SuppressLint("SimpleDateFormat") String time = new SimpleDateFormat("yyyMMdd_hhmmss").format(new Date());
         File photoDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         try {
+
             File photoFile = File.createTempFile("photo" + time, ".jpg", photoDir);
 
             // Saving of the full path
@@ -96,14 +120,37 @@ public class CountingActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            switch (requestCode){
+                case BACK_TAKE_PICTURE:
+                    // When a picture has just been taken
 
-        // Verify the right return code and the state of the return to be ok
-        if(requestCode == BACK_TAKE_PICTURE && resultCode == RESULT_OK){
-            // Getting of the image
-            Bitmap image = BitmapFactory.decodeFile(photoPath);
+                    // Getting of the image
+                    Bitmap image = BitmapFactory.decodeFile(photoPath);
 
-            // Print the image
-            imageViewer.setImageBitmap(image);
+                    // Print the image
+                    imageViewer.setImageBitmap(image);
+                    break;
+
+                case BACK_CHOOSE_PICTURE:
+                    // When we just have selected a picture
+
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                    // Get the cursor
+                    Cursor cursor = getContentResolver().query(selectedImage,
+                            filePathColumn, null, null, null);
+                    // Move to first line
+                    cursor.moveToFirst();
+                    // Get the column index of MediaStore.Images.Media.DATA
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    // Get the string value in the column
+                    String imgDecodableString = cursor.getString(columnIndex);
+                    cursor.close();
+                    // Define the image in ImageView after decoding of the string
+                    imageViewer.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+                    break;
+            }
         }
     }
 }
