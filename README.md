cded503cbaf879bbd37edb780e486ccf657abdc8
Axel Tremaudant <axel.tremaudant@gmail.com>
2023-02-03 13:22:40 +0100

    First writing of the README.md Reorganisation of the activity Upgrading of the interface

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
+[comment]: <> (© 2023 by Collen Leon and Tremaudant Axel)
+
 # SheetCount
  
+TODO: ///////////////// Image app ///////////////////////////
+
+## Table of contents
+
+* [Introduction](#Introduction)
+* [Utilisation](#Utilisation)
+* [Development](#Development)
+
+<span id="Introduction"><span>
+## Introduction
+
+**SheetCount** is an application developed for *Android* to count sheets in an image. The image processing is realised with the library *OpenCV*. The whole development is realised with *Android Studio* in *Java*.
+
+<p align="center">
+<img src="https://3.bp.blogspot.com/-yvrV6MUueGg/ToICp0YIDPI/AAAAAAAAADg/SYKg4dWpyC43AAfrDwBTR0VYmYT0QshEgCPcBGAYYCw/s1600/OpenCV_Logo.png" alt="drawing" height="100">
+<img src="https://borntocode.fr/wp-content/uploads/2016/05/android-studio-home.png" alt="drawing" height="100">
+<img src="https://s3images.coroflot.com/user_files/individual_files/572923_bfaleg9ynacrlnok47k4l8hgp.jpg" alt="drawing" height="100">
+</p>
+
+
+
+<span id="Utilisation"><span>
+## Utilisation
+
+
+
+
+
+<span id="Development"><span>
+## Development
+
+For using the code, you must install *OpenCV*. To do so, we followed the tutorial from [Medium](https://medium.com/android-news/a-beginners-guide-to-setting-up-opencv-android-library-on-android-studio-19794e220f3c).
+
+NOTA:  
+* Step 3 when you select the source location, select *sdk* and not *java*.
+* Step 6 after renaming the folder to *jniLibs*, go into the folder and for each software architecture rename the file to *libopencv_java460.so*.
+
+
--- app/src/main/java/com/ensibs_project/sheetcount/controller/MainActivity.java
+++ app/src/main/java/com/ensibs_project/sheetcount/controller/MainActivity.java
@@ -1,42 +1,232 @@
 /**
  * © 2023 Collen Leon and Tremaudant Axel
  *
- * This file contains the class MainActivity which launch the app.
+ * This file contains the class MainActivity which print the image and deal with everything to count the sheets.
  */
 
 package com.ensibs_project.sheetcount.controller;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.ensibs_project.sheetcount.R;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.ensibs_project.sheetcount.model.FindSheets;
+
 /**
- * Class for the activity at the start of the app
+ * Class for the activity to count the sheets
  */
 public class MainActivity extends AppCompatActivity {
-    private Button startButton;
-    private static final int COUNTING_ACTIVITY_CODE = 41;
+    private ImageView imageViewer;
+    private Button pictureButton;
+    private String photoPath;
+    private Button galleryButton;
+    private static final int BACK_TAKE_PICTURE = 1;
+    private static final int BACK_CHOOSE_PICTURE = 2;
+    private ScaleGestureDetector scaleGestureDetector;
+    private float scaleFactor = 1.0f;
+
     /**
-     * Called at the creation of the activity
-     * @param savedInstanceState description
+     * Set the items
+     * @param savedInstanceStata description
      */
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceStata){
+        super.onCreate(savedInstanceStata);
         setContentView(R.layout.activity_main);
 
-        startButton = findViewById(R.id.startButton);
-        startButton.setEnabled(true);
+        pictureButton = findViewById(R.id.takePictBtn);
+        galleryButton = findViewById(R.id.galleryBtn);
 
-        startButton.setOnClickListener(view -> {
-            Intent countingActivityIntent = new Intent(MainActivity.this, CountingActivity.class);
-            startActivityForResult(countingActivityIntent, COUNTING_ACTIVITY_CODE);
-        });
+        imageViewer = findViewById(R.id.imageView);
+
+        pictureButton.setOnClickListener(view -> takePicture());
+        galleryButton.setOnClickListener(view -> choosePicture());
+
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        scaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
+    }
 
+    /**
+     * Function called to choose a picture in the gallery.
+     * NOTA: SheetCount must be allowed in 'Files and media permission' in 'Settings' of the phone.
+     */
+    private void choosePicture(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+
+        // Define thee type as image/*.
+        // This guarantee than just image type components are selected
+        intent.setType("image/*");
+        // We give an array with MIME types accepted
+        // This guarantee than just MIME type components are targeted
+        String[] mimeTypes = {"image/jpeg", "image/png","image/jpg"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+
+        startActivityForResult(intent, BACK_CHOOSE_PICTURE);
+    }
+
+    /**
+     * Function to open the camera and take a picture
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void takePicture(){
+        // Creation of a window to take a picture
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Creation of an unique filename
+        @SuppressLint("SimpleDateFormat") String time = new SimpleDateFormat("yyyMMdd_hhmmss").format(new Date());
+        File photoDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        try {
+
+            File photoFile = File.createTempFile("photo" + time, ".jpg", photoDir);
+
+            // Saving of the full path
+            photoPath = photoFile.getAbsolutePath();
+
+            // Creation of the URI
+            Uri photoUri = FileProvider.getUriForFile(MainActivity.this, MainActivity.this.getApplicationContext().getPackageName()+
+                    ".provider", photoFile);
+
+            // Transfer URI to the intent to save the picture in a temp file
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+
+            // Open the activity
+            startActivityForResult(intent, BACK_TAKE_PICTURE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Get the image and print it
+     * @param requestCode return code
+     * @param resultCode result code (must be ok)
+     * @param data the data
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            switch (requestCode){
+                case BACK_TAKE_PICTURE:
+                    // When a picture has just been taken
+
+                    // Getting of the image
+
+                    Bitmap image = null;
+                    try {
+                        image = BitmapFactory.decodeFile(FindSheets.drawContours(photoPath));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    // Print the image
+                    imageViewer.setImageBitmap(image);
+                    break;
+
+                case BACK_CHOOSE_PICTURE:
+                    // When we just have selected a picture
+
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                    // Get the cursor
+                    Cursor cursor = getContentResolver().query(selectedImage,
+                            filePathColumn, null, null, null);
+                    // Move to first line
+                    cursor.moveToFirst();
+                    // Get the column index of MediaStore.Images.Media.DATA
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    // Get the string value in the column
+                    String imgDecodableString = cursor.getString(columnIndex);
+                    cursor.close();
+
+                    // Creation of an unique filename
+                    @SuppressLint("SimpleDateFormat") String time = new SimpleDateFormat("yyyMMdd_hhmmss").format(new Date());
+                    File photoDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                    try {
+                        File photoFile = File.createTempFile("photo" + time, ".jpg", photoDir);
+                        // Saving of the full path
+                        photoPath = photoFile.getAbsolutePath();
+
+                        InputStream is;
+                        OutputStream os;
+
+                        try {
+                            is = new FileInputStream(imgDecodableString);
+                            os = new FileOutputStream(photoFile);
+                            byte[] buffer = new byte[1024];
+                            int len;
+                            while ((len = is.read(buffer)) > 0) {
+                                os.write(buffer, 0, len);
+                            }
+                            is.close();
+                            os.close();
+                        }
+                        catch(IOException e){
+                            e.printStackTrace();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+
+                    // Define the image in ImageView after decoding of the string
+                    //imageViewer.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+                    try {
+                        imageViewer.setImageBitmap(BitmapFactory.decodeFile(FindSheets.drawContours(photoPath)));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        /**
+         * Class to change the scale of the image.
+         */
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            scaleFactor *= scaleGestureDetector.getScaleFactor();
+            scaleFactor = Math.max(0.1f, Math.min(scaleFactor, 10.0f));
+            imageViewer.setScaleX(scaleFactor);
+            imageViewer.setScaleY(scaleFactor);
+            return true;
+        }
+    }
 }
